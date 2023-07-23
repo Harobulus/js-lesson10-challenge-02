a69a2cb0eb475438322d1e7a8e6b4e626272130e
Harobulus <harobulus@gmail.com>
2023-07-23 19:27:19 +0100

    Added code creating a method to celebrate

--- js/script.js
+++ js/script.js
@@ -14,7 +14,24 @@ const learning = {
   category: "Front End Development",
   topicImportance: "high",
   hoursThisWeek: 2,
-  weeklyHourGoal: 7
+  weeklyHourGoal: 7,
+
+  // Create New Properties
+  achievedStudyGoal: false,
+  addStudyTime: function (hours) {
+    //Add Values & Create a Conditional Statement
+    this.hoursThisWeek += hours;
+    if (this.hoursThisWeek >= this.weeklyHourGoal) {
+      this.achievedStudyGoal = true;
+      this.celebrate();
+    };
+  },
+  //Add a Method to Celebrate
+  celebrate: function () {
+    body.classList.add("celebrate");
+    success.classList.remove("hide");
+    dance.classList.remove("hide");
+  },
 };
 
 topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
@@ -22,3 +39,5 @@ countElement.innerText = `✔️ I have ${learning.learningGoals.length} learnin
 
 topicElement.classList.remove("hide");
 countElement.classList.remove("hide");
+
+learning.addStudyTime(7);
